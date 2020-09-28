3eb21bf711353c134414f4eace061dcda09cd9b5
Ashish <akarhade5@gmail.com>
2020-09-28 10:11:50 +0530

    preparing model

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 from torchvision import models, datasets, transforms
 from torch import optim
 import matplotlib.pyplot as plt
-
+from collections import OrderedDict
 
 data_dir = "SELECT_YOUR_DATA_DIRECTORY"
 """
@@ -38,3 +38,85 @@ test_data = datasets.ImageFolder(data_dir + '/valid', transform=test_transform)
 train_loader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
 test_loader = torch.utils.data.DataLoader(test_data, batch_size=64)
 
+
+# I am using pre-trained model DenseNet121 for this task
+
+model = models.densenet121(pretrained=True)
+print(model)
+
+for param in model.parameters():
+    param.requires_grad = False
+
+# changing the last layers of DenseNet model
+classifier = nn.Sequential(OrderedDict([
+    ('fc1', nn.Linear(1024, 512)),
+    ('relu1', nn.ReLU()),
+    ('fc2', nn.Linear(512, 256)),
+    ('relu2', nn.Linear(256, 10)),
+    ('output', nn.LogSoftmax(dim=1))
+]))
+
+device = torch.device('cuda') # use cpu if you don't have GPU 
+
+criterion = nn.NLLLoss()
+optimizer = optim.Adam(model.classifier.parameters(), lr=0.001)
+model.to(device)
+
+
+
+## TRAINING
+traininglosses = []
+testinglosses = []
+testaccuracy = []
+totalsteps = []
+epochs = 1
+steps = 0
+running_loss = 0
+print_every = 5
+for epoch in range(epochs):
+    for inputs, labels in trainloader:
+        steps += 1
+        # Move input and label tensors to the default device
+        inputs, labels = inputs.to(device), labels.to(device)
+        
+        optimizer.zero_grad()
+        
+        logps = model.forward(inputs)
+        loss = criterion(logps, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        
+        if steps % print_every == 0:
+            test_loss = 0
+            accuracy = 0
+            model.eval()
+            with torch.no_grad():
+                for inputs, labels in testloader:
+                    inputs, labels = inputs.to(device), labels.to(device)
+                    logps = model.forward(inputs)
+                    batch_loss = criterion(logps, labels)
+                    
+                    test_loss += batch_loss.item()
+                    
+                    # Calculate accuracy
+                    ps = torch.exp(logps)
+                    top_p, top_class = ps.topk(1, dim=1)
+                    equals = top_class == labels.view(*top_class.shape)
+                    accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+            
+            traininglosses.append(running_loss/print_every)
+            testinglosses.append(test_loss/len(testloader))
+            testaccuracy.append(accuracy/len(testloader))
+            totalsteps.append(steps)
+            print(f"Device {device}.."
+                  f"Epoch {epoch+1}/{epochs}.. "
+                  f"Step {steps}.. "
+                  f"Train loss: {running_loss/print_every:.3f}.. "
+                  f"Test loss: {test_loss/len(testloader):.3f}.. "
+                  f"Test accuracy: {accuracy/len(testloader):.3f}")
+            running_loss = 0
+            model.train()
+
+torch.save(model.state_dict(), './tomato_model.pth')
